e9f9ba073b1d0b78cf6367262c7a6a6361bf4a12
gamsa0812 <gamsa0812@gmail.com>
2025-04-30 16:24:03 +0530

    updated user management

--- backend/src/test/java/com/example/pafbackend/PafBackendApplicationTests.java
+++ backend/src/test/java/com/example/pafbackend/PafBackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.pafbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class PafBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
